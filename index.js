15480abee030d21b4941adddc033226f5a4e0a75
erick <erickmedlav@gmail.com>
2020-06-16 18:20:51 -0500

    merge

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./db/schema");
 const resolvers = require("./db/resolvers");
-<<<<<<< HEAD
 const conectarDB = require("./config/db");
 
 //Conectar a la base de datos
 
 conectarDB();
-=======
->>>>>>> b23b2a894d060328a11d9ec2bdcad5e0de5704d8
 
 //Servidor
 
