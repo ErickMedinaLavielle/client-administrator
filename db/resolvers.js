6f15a99bfeb8de67428845a36aa1bfe61ae07525
erick <erickmedlav@gmail.com>
2020-06-16 17:48:28 -0500

    crm

---
+++ db/resolvers.js
@@ -0,0 +1,75 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+  //console.log(usuario);
+  const { id, email, nombre, apellido } = usuario;
+  return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn });
+};
+
+//Resolvers
+
+const resolvers = {
+  Query: {
+    obtenerCliente: () => {
+      "Clientes";
+    },
+  },
+
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+      console.log(input);
+
+      //revisar si el usuario esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya esta registrado");
+      }
+
+      //Hashear password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      //Guardar en BD
+      try {
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      // si el usuario existe
+
+      const existeUsuario = await Usuario.findOne({ email });
+      if (!existeUsuario) {
+        throw new Error("El usuario no existe");
+      }
+
+      //Revisar si el pasword es correcto
+
+      const passwordCorrecto = await bcryptjs.compare(
+        password,
+        existeUsuario.password
+      );
+
+      if (!passwordCorrecto) {
+        throw new Error("El password es incorrecto");
+      }
+
+      //Crear token
+
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, "24h"),
+      };
+    },
+  },
+};
+
+module.exports = resolvers;
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./db/schema");
-const resolvers = require("./db/resolver");
+const resolvers = require("./db/resolvers");
+const conectarDB = require("./config/db");
+
+//Conectar a la base de datos
+
+conectarDB();
 
 //Servidor
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    const miContext = "hola";
-
-    return miContext;
-  },
 });
 
 //Arrancar servidor
